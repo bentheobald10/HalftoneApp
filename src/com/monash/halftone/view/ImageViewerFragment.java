e0fc80b58b0d3310f47ccac0eda206a70f27953a
Ben Theobald <btheobald10@gmail.com>
2014-05-06 12:01:53 +1000

    Updated view  classes

--- src/com/monash/halftone/view/ImageViewerFragment.java
+++ src/com/monash/halftone/view/ImageViewerFragment.java
@@ -4,13 +4,20 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 
 import com.monash.halftone.R;
+import com.monash.halftone.model.Image;
 
 public class ImageViewerFragment extends Fragment implements OnClickListener{
-
+	ImageView ivMain;
+	Button bShare, bLoad, bSave;
+	RadioButton rbFilter;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.image_viewer_fragment, container, false);
@@ -21,6 +28,22 @@ public class ImageViewerFragment extends Fragment implements OnClickListener{
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	private void loadImage(String file){
+		
+	}
+	
+	private void saveImage(String filename){
+		
+	}
+	
+	private void shareImage(){
+		
+	}
+	
+	private void createImage(Image.Filter filter){
 		
 	}
 }
--- src/com/monash/halftone/view/MainFragment.java
+++ src/com/monash/halftone/view/MainFragment.java
@@ -1,17 +1,17 @@
 package com.monash.halftone.view;
 
-import com.monash.halftone.R;
-import com.monash.halftone.R.layout;
-
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.monash.halftone.R;
 
 public class MainFragment extends Fragment implements OnClickListener{
-
+	Button bCamera, bLoadGallery;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +23,14 @@ public class MainFragment extends Fragment implements OnClickListener{
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		
+	}
+	
+	private void setUp(){
+		
+	}
+	
+	private void takePhoto(){
 		
 	}
 
