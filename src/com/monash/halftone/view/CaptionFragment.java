db21e588c851517f309a13ea8786ab6c7cd847cb
Jake Spicer <jcspi1@student.monash.edu>
2014-05-16 11:09:03 +1000

    Added simple error checking for caption length

--- src/com/monash/halftone/view/CaptionFragment.java
+++ src/com/monash/halftone/view/CaptionFragment.java
@@ -4,12 +4,14 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.monash.halftone.R;
 import com.monash.halftone.model.Caption;
@@ -17,72 +19,82 @@ import com.monash.halftone.model.Image;
 import com.monash.halftone.model.Caption.Position;
 
 public class CaptionFragment extends DialogFragment {
-		Caption.Position capPos; 
-		String text;
-		private String mText = "";
-		
+	
+	Caption.Position capPos; 
+	String text;
+	private String mText = "";
+
 	public interface CaptionDialogListener {
-	        public void onDialogPositiveClick(DialogFragment dialog);
-	        public void onDialogNegativeClick(DialogFragment dialog);
+		public void onDialogPositiveClick(DialogFragment dialog);
+		public void onDialogNegativeClick(DialogFragment dialog);
 	}
-	
+
 	CaptionDialogListener mListener;
-	
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        
-        try{
-        	mListener = (CaptionDialogListener) activity;
-        } catch (ClassCastException e){
-        	throw new ClassCastException(activity.toString()
-                    + " must implement CaptionDialogListener");
-        }
-    
-    }
-	
+
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+
+		try{
+			mListener = (CaptionDialogListener) activity;
+		} catch (ClassCastException e){
+			throw new ClassCastException(activity.toString()
+					+ " must implement CaptionDialogListener");
+		}
+
+	}
+
 	@Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the Builder class for convenient dialog construction
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        final CharSequence[] items = {"Above", "Below"};
-        capPos = Position.ABOVE;
-        
-        final EditText input = new EditText(getActivity());
-        input.setInputType(EditorInfo.TYPE_CLASS_TEXT);
-        input.setHint("Caption");
-        builder.setView(input);
-        
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		// Use the Builder class for convenient dialog construction
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		final CharSequence[] items = {"Above", "Below"};
+		capPos = Position.ABOVE;
+
+		final EditText input = new EditText(getActivity());
+		input.setInputType(EditorInfo.TYPE_CLASS_TEXT);
+		input.setHint("Add caption here");
+		builder.setView(input);
+
+
 		builder.setTitle(R.string.dialog_caption)
-               .setPositiveButton(R.string.proceed, new DialogInterface.OnClickListener() {
-                   public void onClick(DialogInterface dialog, int id) {
-					text = input.getText().toString();
+		.setPositiveButton(R.string.proceed, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				text = input.getText().toString();
+				if(text.length() >= 1 && text.length()<= 40){
 					mListener.onDialogPositiveClick(CaptionFragment.this);
-                   }
-               })
-               .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                   public void onClick(DialogInterface dialog, int id) {
-                       mListener.onDialogNegativeClick(CaptionFragment.this);
-                   }
-               })
-        		.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
-        			public void onClick(DialogInterface dialog, int id){
-        				if ("Above".compareTo(items[id].toString()) == 0){
-        					capPos = Caption.Position.ABOVE ;
-        					Log.i("CaptionFragment", "Above");
-        				}else if ("Below".compareTo(items[id].toString()) == 0){
-        					capPos = Caption.Position.BELOW;
-        					Log.i("CaptionFragment", "Below");
-        				}
-        			}
-        		});
-        // Create the AlertDialog object and return it
-        return builder.create();
-    } 
-    public String getText(){
+				}else if(text.length() < 1){
+					Toast.makeText(getActivity(), "Your caption was too short. Please make your caption at least 1 letter long.",
+							Toast.LENGTH_LONG).show();
+				}else if(text.length() > 40){
+					Toast.makeText(getActivity(), "Your caption was too long. Please make your caption is less than 40 characters",
+							Toast.LENGTH_LONG).show();
+				}
+			}
+		})
+		.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				mListener.onDialogNegativeClick(CaptionFragment.this);
+			}
+		})
+		.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id){
+				if ("Above".compareTo(items[id].toString()) == 0){
+					capPos = Caption.Position.ABOVE ;
+					Log.i("CaptionFragment", "Above");
+				}else if ("Below".compareTo(items[id].toString()) == 0){
+					capPos = Caption.Position.BELOW;
+					Log.i("CaptionFragment", "Below");
+				}
+			}
+		});
+		// Create the AlertDialog object and return it
+		return builder.create();
+	} 
+	public String getText(){
 		return text;
-    	
-    }
-    public Caption.Position getPos(){
-    	return capPos;
-    }
+
+	}
+	public Caption.Position getPos(){
+		return capPos;
+	}
 }
